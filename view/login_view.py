e8f0428ce14a635df6a090bc22903329ea4bd78a
Amirmohammad Zarif Shahsavan Nejad <Developerboys@gmail.com>
2024-02-24 16:22:35 +0330

    dropped ttkthemes dependency

--- view/login_view.py
+++ view/login_view.py
@@ -1,7 +1,4 @@
-from ttkthemes import ThemedTk
-from ttkthemes import ThemedStyle
-from tkinter import ttk
-import tkinter as tk
+from tkinter import *
 from controller.user_controller import UserController
 import tkinter.messagebox as msg
 from view.user_view import UserView
@@ -9,32 +6,28 @@ from view.user_view import UserView
 class LoginView:
     def __init__(self):
         self.controller = UserController()
-        self.window = ThemedTk(theme="clearlooks")
-        style = ThemedStyle(self.window)
-        style.set_theme("arc")
-
+        self.window = Tk()
         self.window.geometry("300x190")
         self.window.title("User Login")
 
         # username
-        ttk.Label(self.window, text="Username").pack()
-        self.username = tk.StringVar()
-        ttk.Entry(self.window, textvariable=self.username).pack()
+        Label(self.window, text="Username").pack()
+        self.username = StringVar()
+        Entry(self.window, textvariable=self.username).pack()
 
         # password
-        ttk.Label(self.window, text="Password").pack()
-        self.password = tk.StringVar()
-        ttk.Entry(self.window, textvariable=self.password).pack()
+        Label(self.window, text="Password").pack()
+        self.password = StringVar()
+        Entry(self.window, textvariable=self.password).pack()
 
 
         # login button
-        ttk.Button(self.window, text="Login", command=self.login_click).place(x=125, y=100)
+        Button(self.window, text="Login", command=self.login_click).place(x=125, y=100)
 
         self.window.mainloop()
 
     def login_click(self):
         user, error_message = self.controller.find_by_username_and_password(self.username.get(), self.password.get())
-        print(UserController.find_all())
         UserController.current_user = user
         if user:
             msg.showinfo("Info", "Successful Login")
